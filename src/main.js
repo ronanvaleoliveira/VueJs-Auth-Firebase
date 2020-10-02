6db856118495cec81bb28845b5e16e6a74b1aac1
Ronan Vale de Oliveira <ronanvale@hotmail.com>
2020-10-02 10:51:29 -0300

    Instalação do firebase e chamada do 'createUserWithEmailAndPassword' para criar o usuario

--- src/main.js
+++ src/main.js
@@ -1,8 +1,24 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import firebase from 'firebase'
 
 Vue.config.productionTip = false
+
+var firebaseConfig = {
+  apiKey: "AIzaSyDhG17sDt57HJOH3oyUiwzMELC6PiL4NJ0",
+  authDomain: "rvo-vue-auth-firebase.firebaseapp.com",
+  databaseURL: "https://rvo-vue-auth-firebase.firebaseio.com",
+  projectId: "rvo-vue-auth-firebase",
+  storageBucket: "rvo-vue-auth-firebase.appspot.com",
+  messagingSenderId: "271079180487",
+  appId: "1:271079180487:web:968f450ecc86ca2efe16e2",
+  measurementId: "G-6VD5FGF25D"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
 
 new Vue({
   router,
--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -1,22 +1,40 @@
 <template>
   <div class="sign-up">
     <h1>Crie uma nova conta</h1>
-    <input type="text" placeholder="Email" />
+    <input type="text" placeholder="Email" v-model="email" />
     <br />
-    <input type="password" placeholder="Senha" />
+    <input type="password" placeholder="Senha" v-model="senha" />
     <br />
-    <button>Registrar</button>
+    <button @click="signUp">Registrar</button>
     <span> ou retorne ao <router-link to="/login"> login.</router-link></span>
   </div>
 </template>
 
 <script>
+import firebase from "firebase";
 export default {
   name: "SignUp",
   data() {
-    return {};
+    return {
+      email: "",
+      senha: "",
+    };
   },
-  methods: {},
+  methods: {
+    signUp() {
+      firebase
+        .auth()
+        .createUserWithEmailAndPassword(this.email, this.senha)
+        .then(
+          () => {
+            this.$router.replace("/");
+          },
+          (err) => {
+            alert("Aconteceu algo inesperado. " + err.message);
+          }
+        );
+    },
+  },
 };
 </script>
 
