5dd9f06fe535092d891ce21ecdf5321efe115886
Ronan Vale de Oliveira <ronanvale@hotmail.com>
2020-10-02 09:09:20 -0300

    Início do projeto e configuração das rotas de navegação

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # VueJs-Auth-Firebase
-Neste projeto você verá como criar um projeto Vue.js e fazer a autenticação através do sistema de autenticação Firebase.
+Este projeto será desenvolvido em Vue.js utilizando o sistema de autenticação do Firebase.
+
+## Recursos implementados
+* Rotas de navegação com "vue-router"
+
+## Próximos passos
+* Integração com Firebase
+
+
+
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/views/SignUp.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="sign-up">
+    <h1>Crie uma nova conta</h1>
+    <input type="text" placeholder="Email" />
+    <br />
+    <input type="password" placeholder="Senha" />
+    <br />
+    <button>Registrar</button>
+    <span> ou retorne ao <router-link to="/login"> login.</router-link></span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SignUp",
+  data() {
+    return {};
+  },
+  methods: {},
+};
+</script>
+
+ <style scoped>
+.sign-up {
+  margin-top: 40px;
+}
+input {
+  margin: 10px 0;
+  width: 20%;
+  padding: 15px;
+}
+button {
+  font-family: "Roboto", sans-serif;
+  text-transform: uppercase;
+  outline: 0;
+  background: #1606f3;
+  border: 0;
+  padding: 15px;
+  color: #ffffff;
+  font-size: 14px;
+  -webkit-transition: all 0.3 ease;
+  transition: all 0.3 ease;
+  cursor: pointer;
+}
+span {
+  display: block;
+  margin-top: 20px;
+  font-size: 11px;
+}
+</style>
